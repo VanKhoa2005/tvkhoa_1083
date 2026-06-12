e1126de9a611e84bff2783f715e465e9e8f89962
TranVanKhoa_1083 <tranvankhoa2005dmx@gmail.com>
2026-06-12 18:57:50 +0700

    add

---
+++ lab_01/ex02/ex02_02.py
@@ -0,0 +1,6 @@
+# Nhâp bán kính tu nguoi dung
+ban_kinh = float(input("Nhập bán kính hình tròn: "))
+#Tính diện tích hình tròn
+dien_tich = 3.14 * (ban_kinh**2)
+#In ra diện tích hình tròn
+print("Diện tích hình tròn là: ",dien_tich) 
